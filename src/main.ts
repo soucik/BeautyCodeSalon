89db6d01122fc736b1be2988933ceea61efcef11
Ing. Pavol Šoučík <soucik@gmail.com>
2017-04-13 20:26:57 +0200

    ADDED: production mode

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-// import {enableProdMode} from '@angular/core';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-// enableProdMode();
+import { enableProdMode } from '@angular/core';
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
