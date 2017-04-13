2cb797e879baf2f1848d53381816ecd44b826b7a
Ing. Pavol Šoučík <soucik@gmail.com>
2017-04-13 19:06:19 +0200

    FIXED: google maps clicked label

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // import {enableProdMode} from '@angular/core';
 "use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app/app.module');
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_module_1 = require("./app/app.module");
 // enableProdMode();
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
