1e40b2664150939d1b2b787aba22c38f8ff3cb02
Ing. Pavol Šoučík <soucik@gmail.com>
2017-07-22 21:47:34 +0200

    Changed: HTML5 tags, styling whole page begin

---
+++ src/app/Shared/Shared/shared.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'social-component',
+  templateUrl: './app/Shared/Social/social.html'
+})
+export class SocialComponent{
+
+  constructor() { }
+
+}
