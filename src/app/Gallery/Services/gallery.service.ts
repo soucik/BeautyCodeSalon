9df930f20b240cb0c3a826df13610b1315e5332b
Ing. Pavol Šoučík <soucik@gmail.com>
2017-09-23 20:21:55 +0200

    Changed: Component, Service for facebook Gallery.

--- src/app/Gallery/Services/gallery.service.ts
+++ src/app/Gallery/Services/gallery.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-
+import * as I from '../IGallery';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class GalleryService {
 
-private imagesPath='img/gallery/images.json';
+  private baseUrl = 'http://localhost:8080/BeautyCodeSalon/';
 
-constructor(private http: Http) { }
+  constructor(private http: Http) { }
 
-  getImageNames(): Promise<any> {
-    return this.http.get(this.imagesPath)
-               .toPromise()
-               .then(response => response.json())
-               .catch();
+  RequestPhotosIds(albumId: string): Observable<Array<I.ResponseAlbum>> {
+    return this.http.get(this.baseUrl + 'album/' + albumId)
+      .map((res: any) => {
+        return <Array<I.ResponseAlbum>>JSON.parse(res._body);
+      });
   }
+
+  RequestPhotoUrl(photoId: string, photoType: string = 'album'): Observable<I.ResponsePhotoUrl> {
+    return this.http.get(this.baseUrl + 'photoUrl/' + photoId + '/' + photoType)
+      .map((res: any) => {
+        return <I.ResponsePhotoUrl>JSON.parse(res._body);
+      });
+  }
+
 }
--- src/app/Gallery/gallery.component.ts
+++ src/app/Gallery/gallery.component.ts
@@ -1,53 +1,117 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { GalleryService } from './Services/gallery.service';
 import { ActivatedRoute, Router } from '@angular/router';
-
-interface Images{
-  path: string;
-  name: string;
-  size: number;
-  extension: string;
-}
-
+import * as I from './IGallery';
+import { Observable } from 'rxjs/Rx';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'gallery-salon',
   templateUrl: './app/Gallery/gallery.html',
   providers: [GalleryService]
 })
 
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
 
-  imgPath: string = 'img/gallery/';
-  images: Array<Images>;
   subscribeUrlParam_photo: any;
+
+  public hairAlbum: I.FbAlbum = {
+    albumID: '10204426332601677'
+  };
 
   constructor(
     private galleryService: GalleryService,
+    private sanitizer: DomSanitizer,
     private route: ActivatedRoute,
     private router: Router) {
+
+    this.initiallyGetAlbumPhotos();
   }
 
-  public callImageNames(): void {
-    this.galleryService.getImageNames()
-      .then((data) => {
-        this.images = data.map((data: any) => data.name);
-        this.GetParameters();
+  initiallyGetAlbumPhotos(): Promise<any> {
+    return this.getAlbumPhotos(this.hairAlbum, 'album')
+      .then(albumWithPhotoIds => {
+        this.hairAlbum = this.getPhotosUrls(albumWithPhotoIds, 'album');
       });
   }
 
   ngOnInit() {
-    this.callImageNames();
+    this.GetUrlParameters();
   }
 
-  GetParameters() {
-    this.subscribeUrlParam_photo = this.route.queryParams.subscribe(urlParams => {
-      var photo = +urlParams['photo'];
-      if(!isNaN(photo))
-        this.showModal(photo);
+  getAlbumPhotos(albumWithId: I.FbAlbum, photoType?: string): Promise<I.FbAlbum> {
+
+    return new Promise<I.FbAlbum>((resolve, reject) => {
+      let oAlbumPhotos = this.galleryService.RequestPhotosIds(albumWithId.albumID);
+
+      oAlbumPhotos.subscribe((response: I.ResponseAlbum[]) => {
+        albumWithId.photos = response;
+        albumWithId.photos.forEach((photo: I.Photos) => { photo.photoElements = new Array() });
+        resolve(albumWithId);
+      });
     });
   }
 
-  showModal(photo: number) {
+  getPhotosUrls(albumWithPhotoIds: I.FbAlbum, photoType?: string): I.FbAlbum {
+    albumWithPhotoIds.photos.forEach((photoRecord: I.Photos) => {
+      this.getOnePhotoUrl(photoRecord.id)
+        .subscribe((data: I.ResponsePhotoUrl) => this.assignRecordToAlbum(data));
+    })
+    return albumWithPhotoIds;
+  }
+
+  getOnePhotoUrl(photoId: string, photoType?: string): Observable<any> {
+    return this.galleryService.RequestPhotoUrl(photoId, photoType);
+  }
+
+  GetUrlParameters() {
+    this.subscribeUrlParam_photo = this.route.queryParams.subscribe((urlParams: Array<any>) => {
+      var photoIdToShow: string = urlParams['photo'];
+      if (/^\d+$/.test(photoIdToShow)) {
+        this.testBeforeModal(photoIdToShow);
+      }
+    });
+  }
+
+  testBeforeModal(photoIdToShow: string): void {
+
+    if (this.hairAlbum.photos) {
+      var foundRecord = this.hairAlbum.photos
+        .filter((record: I.Photos) => record.id == photoIdToShow);
+    }
+
+    if (!foundRecord) //No record exists
+    {
+      this.getOnePhotoUrl(photoIdToShow, 'normal')
+        .subscribe((record: I.ResponsePhotoUrl) => {
+          this.showModal(record.url);
+          this.initiallyGetAlbumPhotos()
+            .then(resolve => this.assignRecordToAlbum(record));
+        });
+    }
+
+    if (foundRecord && foundRecord[0].id && foundRecord[0].photoElements[0] && !(foundRecord[0].photoElements[1])) // if Record Exists & Album-PhotoType Exists
+      this.getOnePhotoUrl(foundRecord[0].id, 'normal')
+        .subscribe((record: I.ResponsePhotoUrl) => {
+          this.assignRecordToAlbum(record);
+          this.showModal(foundRecord[0].photoElements[1]);
+        });
+
+    if (foundRecord && foundRecord[0].photoElements[1]) // if Record Exists & Normal-PhotoType Exists
+      this.showModal(foundRecord[0].photoElements[1]);
+  }
+
+  assignRecordToAlbum(record: any) {
+
+    var foundPhotoIndex = this.hairAlbum.photos
+      .findIndex((recordToAssign: I.Photos) => recordToAssign.id == record.id);
+    var photoEl = this.hairAlbum.photos[foundPhotoIndex];
+    if (record.photoType == 'album')
+      photoEl.photoElements[0] = record.url;
+    else
+      photoEl.photoElements[1] = record.url;
+  }
+
+  showModal(url: string) {
     var me = this;
     var $lightbox = $('#lightbox');
     var $img = $lightbox.find('img');
@@ -57,7 +121,7 @@ export class GalleryComponent {
       'maxHeight': $(window).height() - 5
     };
 
-    $img.attr('src', this.imgPath + this.images[photo-1]);
+    $img.attr('src', url);
     $close.toggle();
     $img.css(css).toggle();
     $('#lightbox').modal('show');
